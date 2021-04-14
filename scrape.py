5a1c110bfcb2689060e744527e6a978175004630
iridescent7 <iridescent1337@gmail.com>
2021-04-15 04:24:36 +0700

    import cleanup

--- scrape.py
+++ scrape.py
@@ -5,11 +5,6 @@ import sys
 import time
 
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-
 from progress.bar import Bar
 
 def is_youtube_video_url(video_url):
